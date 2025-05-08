f4c0612fbbbf13abd669dec8f0cc4ae0ccf7b512
Austin <austin@shinzolabs.com>
2025-05-08 10:50:09 -0700

    More gh workflow debugging

--- .github/scripts/validate-no-version-change.sh
+++ .github/scripts/validate-no-version-change.sh
@@ -6,12 +6,8 @@ git fetch origin $BASE_BRANCH
 pkg_version=$(jq -r .version package.json)
 base_version=$(git show origin/$BASE_BRANCH:package.json | jq -r .version)
 
-IFS='.' read -r pkg_major pkg_minor pkg_patch <<EOF
-$pkg_version
-EOF
-IFS='.' read -r base_major base_minor base_patch <<EOF
-$base_version
-EOF
+IFS='.' read -r pkg_major pkg_minor pkg_patch <<< "$pkg_version"
+IFS='.' read -r base_major base_minor base_patch <<< "$base_version"
 
 inc_count=0
 
@@ -24,7 +20,7 @@ elif [ "$pkg_major" -eq "$base_major" ] && [ "$pkg_minor" -eq "$base_minor" ] &&
 fi
 
 if [ "$inc_count" -ne 0 ] && [ "$inc_count" -ne 1 ]; then
-  echo "Error: Version must increment exactly one of major, minor, or patch by 1 (and reset lower segments if major/minor is incremented)."
+  echo "Error: Version must increment at most one of major, minor, or patch by 1 (and reset lower segments if major/minor is incremented)."
   echo "Base branch version: $base_version"
   echo "PR version: $pkg_version"
   exit 1
