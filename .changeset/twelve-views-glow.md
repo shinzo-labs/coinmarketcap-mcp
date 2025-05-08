86e6d2f0565bcf26b914b5edee90b377e583ef55
Austin Born <austin@shinzolabs.com>
2025-05-08 10:54:57 -0700

    Add script to update MCP version by sed match (#36)
    * Add script to update MCP version by sed match
    
    * Make update-mcp-version.sh executable
    
    * Test update package version
    
    * Update script to handle git bot actions
    
    * Add changeset
    
    * Update update-mcp-version.sh script
    
    * Simplify script
    
    * Update MCP version script
    
    * Ensure branch is rebased first
    
    * Update MCP version to 1.3.1
    
    * More gh workflow debugging
    
    * Update validate-no-version-change.sh
    
    * Revert version change
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/twelve-views-glow.md
@@ -0,0 +1,5 @@
+---
+"@shinzolabs/coinmarketcap-mcp": patch
+---
+
+Ensure MCP version can be updated in same release PRs
---
+++ .github/scripts/update-mcp-version.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+version=$(jq -r .version package.json)
+
+sed -i "s/version: \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/version: \"$version\"/g" $PATH_TO_FILE
+
+if git diff --quiet $PATH_TO_FILE; then
+  echo "No changes to MCP version, skipping commit"
+else
+  git config --global user.email "github-actions[bot]@users.noreply.github.com"
+  git config --global user.name "github-actions[bot]"
+  git add $PATH_TO_FILE
+  git commit -m "Update MCP version to $version"
+  branch=$GITHUB_HEAD_REF
+  git fetch origin $branch
+  git pull --rebase origin $branch
+  git push origin HEAD:refs/heads/$branch
+fi
