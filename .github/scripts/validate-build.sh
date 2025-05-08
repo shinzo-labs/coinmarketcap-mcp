d33b859933508fbde7f32d1f954c0f29b46428f8
Austin <austin@shinzolabs.com>
2025-05-07 23:45:28 -0700

    Update workflow checks

--- .github/scripts/validate-build.sh
+++ .github/scripts/validate-build.sh
@@ -3,7 +3,7 @@
 pnpm i
 pnpm build
 
-pnpm install --frozen-lockfile
+pnpm i --frozen-lockfile
 if [ $? -ne 0 ]; then
   echo 'Error: Lockfile is not up to date. Please run `pnpm install` and commit the updated lockfile.'
   exit 1
