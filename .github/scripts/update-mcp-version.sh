19acb91a3384598546faafcf02e998eea9de7e14
Austin <austin@shinzolabs.com>
2025-05-08 09:04:28 -0700

    Update script to handle git bot actions

--- .github/scripts/update-mcp-version.sh
+++ .github/scripts/update-mcp-version.sh
@@ -7,7 +7,10 @@ sed -i "s/version: \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/version: \"$version\"/g" $PATH_
 if git diff --quiet $PATH_TO_FILE; then
   echo "No changes to MCP version, skipping commit"
 else
+  git config --global user.email "github-actions[bot]@users.noreply.github.com"
+  git config --global user.name "github-actions[bot]"
   git add $PATH_TO_FILE
   git commit -m "Update MCP version to $version"
-  git push
+  branch="${GITHUB_REF_NAME:-main}"
+  git push origin HEAD:$branch
 fi
